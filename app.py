a96fac94cab7943cc15f1b52e310bcd633bc1d5b
Parag Kumar Goyal <1997parag.goyal@gmail.com>
2024-07-17 18:34:20 +0530

    add login functionality and db to store users

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room
-
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from db import get_user
 app = Flask(__name__)
+app.secret_key = "my_secret_key"
 socketio = SocketIO(app)
-
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = get_user(username)
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            message = 'Failed to login!'
+
+    return render_template('login.html', message=message)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
 @app.route('/chat')
+@login_required
 def chat():
     username = request.args.get('username')
     room = request.args.get('room')
@@ -32,6 +59,12 @@ def handle_join_room_event(data):
 def send_message(data):
     print("{} has sent message to the room {}: {}".format(data['username'], data['room'], data['message']))
     socketio.emit('receive_message', data, room=data['room'])
+
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
+
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
---
+++ db.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
+import certifi
+from user import User
+
+client = MongoClient("mongodb+srv://1997paraggoyal:parag@mychatcluster.4q5dz4s.mongodb.net/?retryWrites=true&w=majority&appName=MyChatCluster", tlsCAFile=certifi.where())
+
+chat_db = client.get_database("ChatDB")
+users_collection = chat_db.get_collection("users")
+
+def save_user(username, email, password):
+    password_hash = generate_password_hash(password)
+    users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+
+def get_user(username):
+    user_data = users_collection.find_one({'_id': username})
+    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
+
